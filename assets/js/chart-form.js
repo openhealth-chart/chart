b874a0fa7ca39d8ea06d2ed444f1baa86cd2bb7f
Jonathan Borden <jonathan@openhealth.org>
2024-08-24 13:05:43 -0400

    Update chart-form.js

--- assets/js/chart-form.js
+++ assets/js/chart-form.js
@@ -1,9 +1,9 @@
 
-function appendPath(url, path) {
+export function appendPath(url, path) {
     return url.endsWith('/') ? url + path : url + '/' + path;
     }
 
-function submitQuestion(form, url,loc,accessToken,taskId) {
+export function submitQuestion(form, url,loc,accessToken,taskId) {
     chartRecorder.showLoading();
 
     const formData = new FormData(form);
@@ -16,7 +16,7 @@ function submitQuestion(form, url,loc,accessToken,taskId) {
         sendRequest(appendPath(url,loc), data,accessToken,taskId, handleQuestionResponse);
     }
 
-function sendRequest(url, data, accessToken, taskId,responseHandler = handleFormResponse) {
+export function sendRequest(url, data, accessToken, taskId,responseHandler = handleFormResponse) {
             console.log(`Submitting to ${url}:`, JSON.stringify(data));
             fetch(url, {
                 method: 'POST',
@@ -36,20 +36,20 @@ function sendRequest(url, data, accessToken, taskId,responseHandler = handleForm
                 });
 }
 
-    function handleFormResponse(html) {
+export  function handleFormResponse(html) {
         console.log("response:",html);
         document.open();
         document.write(html);
         document.close();
     }
 
-    function handleQuestionResponse(result) {
+ export   function handleQuestionResponse(result) {
         let responseText = typeof result === 'string' ? JSON.parse(result).answer || result : JSON.stringify(result);
         console.log('Received response:', responseText);
         appendResponse(document.getElementById('conversation'), responseText);
     }
 
-    function handleError(error) {
+ export   function handleError(error) {
         console.error('Error:', error);
         alert('An error occurred: ' + error.message);
     }
